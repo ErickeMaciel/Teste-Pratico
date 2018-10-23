80e5f9a75341dc31df5e392e71210bdb8be01508
Éricke <erickemaciel@gmail.com>
2018-10-22 23:10:39 -0300

    Initial Commit

---
+++ Teste pratico/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    public Material objMat;
+    public Renderer[] objMatChilds;
+    
+  
+	// Use this for initialization
+	void Start ()
+    {
+        // Buscar o material existente do objeto
+        objMat = GetComponent<Renderer>().material;
+        objMat.color = Color.black;
+
+        // Buscar o componente renderer de todos os objetos filhos
+        objMatChilds = GetComponentsInChildren<Renderer>();
+
+     
+        // Acessar o material dos objetos filhos e substituir pelo mesmo do pai
+        foreach(Renderer rend in objMatChilds)
+        {
+            var mats = new Material[rend.materials.Length];
+            for(var j = 0; j < rend.materials.Length; j++)
+            {
+                mats[j] = objMat;
+            }
+            rend.materials = mats;
+          
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Funcao para trocar a cor do objeto
+    public void ObjColor()
+    {
+        if(objMat.color == Color.black)
+        {
+            objMat.color = Color.white;
+        }
+        else if (objMat.color == Color.white)
+        {
+            objMat.color = Color.red;
+        }
+        else if (objMat.color == Color.red)
+        {
+            objMat.color = Color.blue;
+        }
+        else if (objMat.color == Color.blue)
+        {
+            objMat.color = Color.black;
+        }
+
+
+
+    }
+}
---
+++ Teste pratico/Assets/Scripts/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+
+    public float anglesRot = -30.0f;
+    public float rotTemp;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Funcao de rotacao do objeto
+    public void RotateObject()
+    {
+        rotTemp -= anglesRot;
+
+        transform.eulerAngles = new Vector3(0, rotTemp, 0);
+    }
+}
